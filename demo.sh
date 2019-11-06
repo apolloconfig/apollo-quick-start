dcafbb8c6f3c0ce5217d36f05ed4abcdf0b4eca1
lixin2976290267 <52155614+lixin2976290267@users.noreply.github.com>
2019-11-06 19:44:15 +0800

    ss
    ss

--- demo.sh
+++ demo.sh
@@ -3,12 +3,12 @@
 # apollo config db info
 apollo_config_db_url=jdbc:mysql://localhost:3306/ApolloConfigDB?characterEncoding=utf8
 apollo_config_db_username=root
-apollo_config_db_password=
+apollo_config_db_password=root
 
 # apollo portal db info
 apollo_portal_db_url=jdbc:mysql://localhost:3306/ApolloPortalDB?characterEncoding=utf8
 apollo_portal_db_username=root
-apollo_portal_db_password=
+apollo_portal_db_password=root
 
 # =============== Please do not modify the following content =============== #
 
