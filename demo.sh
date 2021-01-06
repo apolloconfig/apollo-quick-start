a93f3c68ab07651276b1ba426cabcd5a4b77ebff
Jason Song <songshun.ss@antfin.com>
2021-01-06 10:41:41 +0800

    Revert "add serverTimezone configuration in case user's mysql is not correctly setup"
    This reverts commit 5809ef09ecbd407d1d8957fa72a7c75ec3d538ec.

--- demo.sh
+++ demo.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 
 # apollo config db info
-apollo_config_db_url="jdbc:mysql://localhost:3306/ApolloConfigDB?characterEncoding=utf8&serverTimezone=Asia/Shanghai"
+apollo_config_db_url=jdbc:mysql://localhost:3306/ApolloConfigDB?characterEncoding=utf8
 apollo_config_db_username=root
 apollo_config_db_password=
 
 # apollo portal db info
-apollo_portal_db_url="jdbc:mysql://localhost:3306/ApolloPortalDB?characterEncoding=utf8&serverTimezone=Asia/Shanghai"
+apollo_portal_db_url=jdbc:mysql://localhost:3306/ApolloPortalDB?characterEncoding=utf8
 apollo_portal_db_username=root
 apollo_portal_db_password=
 
